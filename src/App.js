74fb2f7c960bbdbb38ff954904ccb4d0c213c3a6
missvader <vanepalazon611@gmail.com>
2022-12-05 12:51:39 +0100

    form submit event done, create object todo and save state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [inputText, setInputText] = useState("")
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Vanessa's Todo List</h1>
       </header>
-      <Form setInputText={setInputText}/>
+      <Form 
+        inputText = {inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}/>
       <TodoList/>
     </div>
   );
